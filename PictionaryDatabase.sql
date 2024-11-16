5791c796c7a7a3bbe34dd7a912e247cfd358f48f
Graham <166065353+ChocolatechipGr@users.noreply.github.com>
2024-11-16 15:45:59 -0600

    Add files via upload

---
+++ PictionaryDatabase.sql
@@ -0,0 +1,56 @@
+create table PictionaryThemes (
+Theme_Number int(8) auto_increment primary key,
+Theme varchar(30) );
+
+insert into PictionaryThemes (Theme) values('Eiffel Tower');
+insert into PictionaryThemes (Theme) values('The Mona Lisa');
+insert into PictionaryThemes (Theme) values('T-Rex');
+insert into PictionaryThemes (Theme) values('Harry Potter');
+insert into PictionaryThemes (Theme) values('Pizza');
+insert into PictionaryThemes (Theme) values('Octopus');
+insert into PictionaryThemes (Theme) values('Rainbow');
+insert into PictionaryThemes (Theme) values('Hot Dog');
+insert into PictionaryThemes (Theme) values('Moon Landing');
+insert into PictionaryThemes (Theme) values('Snowman');
+insert into PictionaryThemes (Theme) values('Spider-Man');
+insert into PictionaryThemes (Theme) values('Wedding Cake');
+insert into PictionaryThemes (Theme) values('Toothbrush');
+insert into PictionaryThemes (Theme) values('Kangaroo');
+insert into PictionaryThemes (Theme) values('UFO');
+insert into PictionaryThemes (Theme) values('Statue of Liberty');
+insert into PictionaryThemes (Theme) values('Pirate Ship');
+insert into PictionaryThemes (Theme) values('Hamburger');
+insert into PictionaryThemes (Theme) values('Leaning Tower of Pisa');
+insert into PictionaryThemes (Theme) values('Cactus');
+insert into PictionaryThemes (Theme) values('Giraffe');
+insert into PictionaryThemes (Theme) values('Treehouse');
+insert into PictionaryThemes (Theme) values('Titanic');
+insert into PictionaryThemes (Theme) values('Snowstorm');
+insert into PictionaryThemes (Theme) values('Soccer Ball');
+insert into PictionaryThemes (Theme) values('Disco Ball');
+insert into PictionaryThemes (Theme) values('Birthday Cake');
+insert into PictionaryThemes (Theme) values('Dragon');
+insert into PictionaryThemes (Theme) values('Ballerina');
+insert into PictionaryThemes (Theme) values('Police Officer');
+insert into PictionaryThemes (Theme) values('Treasure Chest');
+insert into PictionaryThemes (Theme) values('Ice Cream Cone');
+insert into PictionaryThemes (Theme) values('Roller Coaster');
+insert into PictionaryThemes (Theme) values('Magic Wand');
+insert into PictionaryThemes (Theme) values('Light Bulb');
+insert into PictionaryThemes (Theme) values('Wizard');
+insert into PictionaryThemes (Theme) values('Sandcastle');
+insert into PictionaryThemes (Theme) values('Pirate Hat');
+insert into PictionaryThemes (Theme) values('Tornado');
+insert into PictionaryThemes (Theme) values('Traffic Light');
+insert into PictionaryThemes (Theme) values('UFO Abduction');
+insert into PictionaryThemes (Theme) values('Bowling Ball');
+insert into PictionaryThemes (Theme) values('Cup of Coffee');
+insert into PictionaryThemes (Theme) values('Violin');
+insert into PictionaryThemes (Theme) values('Sunglasses');
+insert into PictionaryThemes (Theme) values('Beach Umbrella');
+insert into PictionaryThemes (Theme) values('Haunted House');
+insert into PictionaryThemes (Theme) values('Snowflake');
+insert into PictionaryThemes (Theme) values('Vacuum Cleaner');
+
+
+select * from PictionaryThemes;
